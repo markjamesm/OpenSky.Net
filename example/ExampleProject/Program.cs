a2bf4e66eec5ae0ca4fe5bb5e6b747cdc07e8534
Mark-James McDougall <contact@markjames.dev>
2025-10-21 23:44:12 -0400

    Add example project.

---
+++ example/ExampleProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenSky.Net;
+
+namespace ExampleProject;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var openSkyApi = new OpenSkyClient(10);
+        var vecs = await openSkyApi.GetAllStateVectors();
+        
+        Console.WriteLine(vecs);
+    }
+}
