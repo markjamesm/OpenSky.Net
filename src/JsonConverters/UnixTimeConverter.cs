94e58e021005b8616c7df848fd5119a6f71a42b5
Mark-James McDougall <contact@markjames.dev>
2025-10-25 13:16:00 -0400

    Update namespace of UnixTimeConverter.cs

--- src/JsonConverters/UnixTimeConverter.cs
+++ src/JsonConverters/UnixTimeConverter.cs
@@ -1,7 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace OpenSky.Net.Converters;
+namespace OpenSky.Net.JsonConverters;
 
 public class UnixTimeConverter : JsonConverter<DateTimeOffset>
 {
