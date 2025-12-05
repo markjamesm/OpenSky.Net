abb0932c04162c7cc16e0cf06d3a05b84dd3df6c
Mark-James McDougall <contact@markjames.dev>
2025-12-04 20:23:39 -0500

    Update examples.

--- example/ExampleProject/Program.cs
+++ example/ExampleProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OpenSky.Net;
+using OpenSky.Net.Models;
 
 namespace ExampleProject;
 
@@ -7,8 +8,31 @@ class Program
     static async Task Main(string[] args)
     {
         var openSkyApi = new OpenSkyClient(10);
-        var vecs = await openSkyApi.GetAllStateVectors();
         
-        Console.WriteLine(vecs);
+        var sr = new StateRequest
+        {
+            Extended = 1,
+            Lamin = 45.8389f,
+            Lomin = 5.9962f,
+            Lomax = 10.5226f, 
+            Lamax = 47.8229f
+        };
+
+        var fr = new FlightRequest
+        {
+            Begin = 1764896859,
+            End = 1764896759,
+        };
+        
+        
+        // Get all state vectors & print them.
+        var vecs = await openSkyApi.GetAllStateVectorsAsync(sr);
+
+        Console.WriteLine(vecs.Time);
+
+        foreach (var i in vecs.StateVectors)
+        {
+            Console.WriteLine($"({i.Callsign} {i.LastContactTime})");
+        } 
     }
 }
